4e25956236b371c6f9dd8ac8e6979b586d254835
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2019-10-23 21:55:37 +0300

    Initial Commit

---
+++ WeatherLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherLogger
+//
+//  Created by Ali Emre Değirmenci on 23.10.2019.
+//  Copyright © 2019 Ali Emre Değirmenci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
